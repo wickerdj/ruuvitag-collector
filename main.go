4ba9d05b59a751c28a25b989727a569d630b3a08
Wick <wickerdj@gmail.com>
2020-02-27 17:54:38 -0600

    basic functionality

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
+	influx "github.com/influxdata/influxdb1-client/v2"
 	"github.com/paypal/gatt"
 	"github.com/paypal/gatt/examples/option"
 	"github.com/wickerdj/ruuvitag-collector/pkg/sensor"
@@ -31,9 +33,47 @@ func onDiscovery(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
 		fmt.Printf("\tAddr: %v\n\tTemperature: %v\n\tHumidity: %v\n\tBattery: %v\n\tTimestamp: %v\n", d.Addr, d.Temperature, d.Humidity, d.Battery, d.Timestamp)
 	}
 
+	write(d)
+}
+
+func write(data sensor.Data) {
+	c, err := influx.NewHTTPClient(influx.HTTPConfig{
+		Addr: "http://192.168.1.204:8086",
+	})
+	if err != nil {
+		fmt.Println("Error creating InfluxDB Client: ", err.Error())
+	}
+	defer c.Close()
+
+	bp, err := influx.NewBatchPoints(influx.BatchPointsConfig{
+		Database:  "ruuvi",
+		Precision: "s",
+	})
+
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+
+	point, err := influx.NewPoint("readings", map[string]string{
+		"mac":  strings.ToUpper(data.Addr),
+		"name": data.Name,
+	}, map[string]interface{}{
+		"temperature":    data.Temperature,
+		"humidity":       data.Humidity,
+		"pressure":       data.Pressure,
+		"battery":        data.Battery,
+		"acceleration_x": data.AccelerationX,
+		"acceleration_y": data.AccelerationY,
+		"acceleration_z": data.AccelerationZ,
+	}, data.Timestamp)
+
+	bp.AddPoint(point)
+
+	c.Write(bp)
 }
 
 func main() {
+
 	device, err := gatt.NewDevice(option.DefaultClientOptions...)
 	if err != nil {
 		log.Fatalf("Problem with device err: %s\n", err)
